74674a27fe504b6261076d28296d47fb3980280a
Hicham-Hal <halouanehicham@gmail.com>
2026-08-31 22:14:23 +0100

    adding the route middleware for app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import userRoute from './routes/user.route.js'
 import cookieParser from 'cookie-parser'
+import planRoute from './routes/workout.route.js'
 
 const app = express()
 
@@ -8,5 +9,6 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/', userRoute)
+app.use('/plan', planRoute)
 
 export default app
