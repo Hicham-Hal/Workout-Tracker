1368ae59712df61cbf2960b79059cb72a15806d3
Hicham-Hal <halouanehicham@gmail.com>
2026-09-01 17:18:20 +0100

    adding getPlans , addComment routes

--- routes/workout.route.js
+++ routes/workout.route.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { addPlan, deletePlan, updatePlan } from '../controllers/workout.controller.js'
+import { addComment, addPlan, deletePlan, getPlans, updatePlan } from '../controllers/workout.controller.js'
+import { verifyToken } from '../middlewares/verifyToken.js'
 
 const route = express.Router()
 
-route.post('/add', addPlan)
-route.put('/update/:id', updatePlan)
-route.delete('/delete', deletePlan)
+route.post('/add', verifyToken, addPlan)
+route.put('/update/:id', verifyToken, updatePlan)
+route.delete('/delete', verifyToken, deletePlan)
+route.post('/:id', verifyToken, addComment)
+route.get('/', verifyToken, getPlans)
 
 export default route
