ad5a8a37589596fbb2b7b372d3f090a51bc7e951
Hicham-Hal <halouanehicham@gmail.com>
2026-09-01 16:13:38 +0100

    token verification function

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import { configDotenv } from 'dotenv'
+
+configDotenv()
+
+export async function verifyToken(req, res, next){
+    try{
+        const token = req.headers['authorization']?.split(' ')[1]
+        if(!token) return res.status(401).json({ message: 'UnAuthorized' })
+        const verify = await jwt.verify(token, process.env.ACCESS_TOKEN)
+        req.user = verify
+        next()
+    }catch(err){
+        console.log(err)
+        return res.status(403).json({ error: 'Forbidden' })
+    }
+}
