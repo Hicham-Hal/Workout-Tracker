79add78ea469cc8d2943b75696ae3a536de67990
Hicham-Hal <halouanehicham@gmail.com>
2026-09-01 17:17:12 +0100

    updating the status

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -7,7 +7,7 @@ export async function verifyToken(req, res, next){
     try{
         const token = req.headers['authorization']?.split(' ')[1]
         if(!token) return res.status(401).json({ message: 'UnAuthorized' })
-        const verify = await jwt.verify(token, process.env.ACCESS_TOKEN)
+        const verify = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
         req.user = verify
         next()
     }catch(err){
