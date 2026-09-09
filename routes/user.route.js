b78f165c389f6102040170ae30d02816c69c0325
Hicham-Hal <halouanehicham@gmail.com>
2026-09-09 14:25:19 +0100

    adding the validators to login and register routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { login, register, logout } from '../controllers/user.controller.js'
+import { loginValidators, registerValidators } from '../validators/auth.validator.js'
+import { validate } from '../validators/validate.js'
 
 const route = express.Router()
 
-route.post('/login', login)
-route.post('/register', register)
+route.post('/login', loginValidators, validate, login)
+route.post('/register', registerValidators, validate, register)
 route.post('/logout', logout)
 
 export default route
