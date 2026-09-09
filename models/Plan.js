f7c79d1acce11d98d12dd815d486fa968d994610
Hicham-Hal <halouanehicham@gmail.com>
2026-09-09 14:24:35 +0100

    updating the Plan model (comment part)

--- models/Plan.js
+++ models/Plan.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 
 const commentSchema = new mongoose.Schema({
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    // author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     comment: { type: String , required: true}
 }, {timestamps: true})
 
