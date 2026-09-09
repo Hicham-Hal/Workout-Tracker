7b30de56a26afa2f2fb6f90fa505775caca2925b
Hicham-Hal <halouanehicham@gmail.com>
2026-09-09 14:27:49 +0100

    auth validators (login, register)

---
+++ validators/auth.validator.js
@@ -0,0 +1,30 @@
+import { body } from "express-validator";
+
+export const registerValidators = [
+    body('username')
+        .trim()
+        .notEmpty().withMessage('Username is required')
+        .isLength({ min: 3, max: 50 }).withMessage('Username must be between 3 and 50 characters'),
+
+    body('email')
+        .trim()
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email must be a valid email address')
+        .normalizeEmail(),
+
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
+        .matches(/\d/).withMessage('Password must contain at least one number')
+]
+
+export const loginValidators = [
+    body('email')
+        .trim()
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email must be a valid email address')
+        .normalizeEmail(),
+
+    body('password')
+        .notEmpty().withMessage('Password is required')
+]
