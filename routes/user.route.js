39cda063c8df1dd961822ce2fa978c2a2b2b9837
Hicham-Hal <halouanehicham@gmail.com>
2026-09-09 14:55:09 +0100

    refresh-token route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, register, logout } from '../controllers/user.controller.js'
+import { login, register, logout, refreshToken } from '../controllers/user.controller.js'
 import { loginValidators, registerValidators } from '../validators/auth.validator.js'
 import { validate } from '../validators/validate.js'
 
@@ -8,5 +8,6 @@ const route = express.Router()
 route.post('/login', loginValidators, validate, login)
 route.post('/register', registerValidators, validate, register)
 route.post('/logout', logout)
+route.post('/refresh-token', refreshToken)
 
 export default route
