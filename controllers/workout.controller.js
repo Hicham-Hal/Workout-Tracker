c032a105742350318ccf9ff4f653a79aaf31482a
Hicham-Hal <halouanehicham@gmail.com>
2026-09-21 13:35:22 +0100

    updating the workout controller

--- controllers/workout.controller.js
+++ controllers/workout.controller.js
@@ -83,7 +83,6 @@ export const deleteComment = async(req, res) => {
         if(!plan) return res.status(404).json({ message: 'plan not found' })
         const comment = plan.comments.find(c => c._id.toString() === id.toString())
         if(!comment) return res.status(404).json({ message: "Comment not found" })
-        // if((req.user.id.toString() !== comment.author.toString()) && (req.user.id.toString() !== plan.owner.toString())) return res.status(403).json({ message: 'you can not delete user\' else comment' })
         plan.comments = plan.comments.filter(c => c._id.toString() !== comment._id.toString())
         await plan.save()
         return res.status(204).send()
@@ -209,7 +208,7 @@ export const getReport = async(req, res) => {
 export const getPlan = async(req, res) => {
     const {id} = req.params
     try{
-        const plan = await Plan.find({ _id: id, owner: req.user.id })
+        const plan = await Plan.findOne({ _id: id, owner: req.user.id })
         if(!plan) return res.status(404).json({ message: "plan not found" })
         return res.status(200).json({ plan })
     }catch(err){
